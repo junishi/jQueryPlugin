86ec9d77b98b84fdb66391320031c455ad4b3dea
niwaringo <niwaringo@gmail.com>
2012-01-27 00:24:31 +0900

    data attr ver

--- jquery.fhconvert/jquery.fhconvert.js
+++ jquery.fhconvert/jquery.fhconvert.js
@@ -1,34 +1,35 @@
 /*
-*  fhconvertのラッププラグインです。
+*  fhconvert(http://distraid.co.jp/demo/js_codeconv.html)のラッププラグインです。
+*  @example HTML <input type="text" data-fhconvert='{"type": "ftoh"}' />
 *  @example $(selector).fhconvert('htof')
 *  @author @niwaringo
 */
 (function($) {
-   var name_space = 'fhconvert';
     /*
-    *  @param {String} type 指定された変換方法で変換
-    *  @param {Object} options 日本語、全角スペース等の設定
-    *
     * ftoh　:　全角英数字→半角
     * htof　:　半角英数字→全角
     * hgtokk　:　ひらがな→全角カタカナ
     * hkktofkk　:　半角ｶﾅ→全角カナ
     */
 
-   $.fn[name_space] = function(type, options) {
-       var $obj = this;
-
-       var settings = $.extend({
-           'jaCode':true,
-           'space':true,
-           'convSet':'object'
-       }, options);
-
-       $obj.each(function() {
+   $.fn.fhconvert = function() {
+       $(this).each(function() {
            var $this = $(this);
-           var convertText = FHConvert[type]($this.val(), settings);
+           //data属性から変換タイプを取得する。
+           var convertType = $this.data('fhconvert').type;
+           //data属性からオプションを取得する。
+           var settings = $.extend({
+               'jaCode':true,
+               'space':true,
+               'convSet':'object'
+           }, $this.data('fhconvert').option);
 
-           $this.val(convertText);
+           if(typeof(convertType) === 'undefined') {
+               return ture;
+           }
+
+           // hfconvertに渡す
+           $this.val(FHConvert[convertType]($this.val(), settings));
        });
 
        return this;
